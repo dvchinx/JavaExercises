339b6888d55e30c0416329a61c6f6ee624538a9d
Usuario <Usuario@DESKTOP-46F3GN9>
2023-11-06 20:11:37 -0500

    Version 1.0

--- src/Intermediate/Ejercicio1/Div.java
+++ src/Intermediate/Ejercicio1/Div.java
@@ -1,12 +1,8 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package Intermediate.Ejercicio1;
 
 /**
  *
- * @author Usuario
+ * @author dvchinx
  */
 public class Div {
 
