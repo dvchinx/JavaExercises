70f89a0eccfc8b643e495e39715b46d14cccad45
ElChino01 <davidflorez0111@gmail.com>
2023-11-12 12:41:58 -0500

    Add Intermediate-Exercise2

---
+++ src/Intermediate/Exercise2/Ejercicio2ES.md
@@ -0,0 +1,28 @@
+<h2>INTERMEDIO -> EJERCICIO 2</h2>
+
+**CONTADOR DE CARACTERES EN UNA CADENA:**
+
+1- Recibir entrada del usuario con la libreria *Java.Util.Scanner*.
+
+2- Crear un *método:(contarCaracter)* que reciba un *String:(cadena)* y retorne un *int*.
+
+3- Las entradas del usuario se deben almacenar como *String:(cadena, caracter)*.
+
+4- La cantidad de cáracteres contados mostrados por consola debe ser *int(nChars)*.
+
+*Tener en cuenta:*
+* *Prevenir posibles errores de entrada por parte del usuario.*
+* *Hacer uso del bucle:(for) y los métodos:[equals() y length()].*
+
+<h2>EJEMPLO:</h2>
+
+**Consola:**
+
+```
+-Contador de Carácter en una Cadena-
+
+Ingresa la Cadena: Jezz y Mel paseaban por el parque felices.
+Ingresa el Carácter a Contar: e
+
+La Cadena Cuenta con: 7 'e'.
+```
---
+++ src/Intermediate/Exercise2/SolutionES/Exec.java
@@ -0,0 +1,60 @@
+package Intermediate.Exercise2.SolutionES;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author dvchinx
+ */
+public class Exec {
+
+    private static Scanner scan = new Scanner(System.in);
+    //Libreria para recibir entrada del usuario.
+
+    private static String cadena, caracter;
+    //Almacena la cadena original ingresada por el usuario.
+    //Almacena el caracter original ingresado por el usuario.
+
+    private static int nChars;
+    //Almacena el número de cáracteres encontrados en la cadena.
+
+    public static void main(String[] args) {
+        System.out.println("-Contador de Carácter en una Cadena-\n");
+
+        System.out.print("Ingresa la Cadena: ");
+        cadena = scan.nextLine();
+
+        System.out.print("Ingresa el Carácter a Contar: ");
+        caracter = scan.next();
+
+        nChars = contarCaracter(cadena);
+
+        System.out.println("\nLa Cadena Cuenta con: "+nChars+" '"+caracter+"'.");
+    }
+
+    public static int contarCaracter(String cadena) {
+        int count = 0;
+        String letter;
+
+        if (caracter.length() == 1) { 
+        //Si String:(caracter) es un solo cáracter, entonces continuar
+            for (int i = 0; i < cadena.length(); i++) { 
+            //Se recorre cada cáracter de la cadena
+                letter = cadena.substring(i, i + 1); 
+                //Letter es igual al cáracter recorrido en el momento
+                if (letter.equals(caracter)) { 
+                //Se compara el cáracter del momento con el que se cuenta
+                    count++;
+                    //Se aumenta en uno la cantidad de cáracteres encontrados.
+                }
+            }
+
+        } else { 
+        //Si String:(cadena) no es un solo cáracter, imprimir error y forzar cierre.
+            System.out.println("\nError, debes agregar un solo carácter.");
+            System.exit(0);
+        }
+        return count;
+
+    }
+}
